50c70ce1c80d92353115e1d9c3aab476a96bd993
darthur2 <david.b.arthur.90@gmail.com>
2026-09-17 12:59:38 -0700

    fixing container startup

--- .devcontainer/start-services.sh
+++ .devcontainer/start-services.sh
@@ -81,7 +81,7 @@ fi
 write_opencode_auth
 
 if ! is_listening "$OPENCODE_PORT"; then
-  start_background_service "opencode web" "$OPENCODE_LOG" opencode web --host 127.0.0.1 --port "$OPENCODE_PORT"
+  start_background_service "opencode web" "$OPENCODE_LOG" opencode web --hostname 127.0.0.1 --port "$OPENCODE_PORT"
 fi
 
 if ! is_listening "$STREAMLIT_PORT"; then
